6cd9652aa29e56a47eee1aa8207d87b2c9df07fa
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-02 17:54:55 +0200

    added new class MainScreen.java

---
+++ src/test/java/screenObjects/MainScreen.java
@@ -0,0 +1,8 @@
+package screenObjects;
+
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+
+public class MainScreen {
+    public static By buttonProfile = MobileBy.id("com.giphy.messenger:id/profile_button");
+}
