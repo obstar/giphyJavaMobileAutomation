2f4f5f62e67022948998c571bbf0305f6740d493
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-02 18:49:01 +0200

    fixing id for password field

--- src/test/java/screenObjects/SignUpLogInScreen.java
+++ src/test/java/screenObjects/SignUpLogInScreen.java
@@ -6,6 +6,6 @@ import org.openqa.selenium.By;
 public class SignUpLogInScreen {
     public static By buttonLogIn = MobileBy.id("com.giphy.messenger:id/startSignUp");
     public static By inputEmail = MobileBy.id("com.giphy.messenger:id/signupEmailEditText");
-    public static By inputPassword = MobileBy.id("com.giphy.messenger:id/signupEmailEditText");
+    public static By inputPassword = MobileBy.id("com.giphy.messenger:id/signupPasswordEditText");
     public static By tabLogIn = MobileBy.AccessibilityId("Log In");
 }
