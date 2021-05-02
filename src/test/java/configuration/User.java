9ba69b03b3341796f7938b1e56e94dcbbb79f7c2
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-02 18:32:20 +0200

    added new class User.java

---
+++ src/test/java/configuration/User.java
@@ -0,0 +1,6 @@
+package configuration;
+
+public final class User {
+    public final static String ExistingEmail = "o.b.star@wp.pl";
+    public final static String ExistingPassword = "P4ssword";
+}
