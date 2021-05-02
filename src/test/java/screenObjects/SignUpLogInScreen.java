8586fdacd8307ff22bfcc77f8d70baa0ac196f1c
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-02 17:56:53 +0200

    added new class SignUpLogInScreen.java

---
+++ src/test/java/screenObjects/SignUpLogInScreen.java
@@ -0,0 +1,11 @@
+package screenObjects;
+
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+
+public class SignUpLogInScreen {
+    public static By buttonLogIn = MobileBy.id("com.giphy.messenger:id/startSignUp");
+    public static By inputEmail = MobileBy.id("com.giphy.messenger:id/signupEmailEditText");
+    public static By inputPassword = MobileBy.id("com.giphy.messenger:id/signupEmailEditText");
+    public static By tabLogIn = MobileBy.AccessibilityId("Log In");
+}
