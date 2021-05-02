add5d53f8b6f01870388575ec783023463386fd9
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-02 18:21:21 +0200

    added new class MainScreenSteps.java

---
+++ src/test/java/steps/MainScreenSteps.java
@@ -0,0 +1,17 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import screenActions.MainScreenActions;
+
+public class MainScreenSteps {
+    public MainScreenActions mainScreenActions;
+
+    public MainScreenSteps(MainScreenActions mainScreenActions) {
+        this.mainScreenActions = mainScreenActions;
+    }
+
+    @Given("user go to Login screen")
+    public void userGoToLoginScreen() {
+        mainScreenActions.goToLoginScreen();
+    }
+}
