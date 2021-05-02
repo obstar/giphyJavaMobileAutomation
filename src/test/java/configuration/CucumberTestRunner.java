009d78189f1b3df36ab76fb2e0fe7c7d6974b8cf
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-02 17:53:07 +0200

    added new class CucumberTestRunner.java

---
+++ src/test/java/configuration/CucumberTestRunner.java
@@ -0,0 +1,25 @@
+package configuration;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "summary", "json:target/report/cucumber.json", "junit:target/report/cucumber.xml"},
+        features = {"src/test/resources/features/"},
+        glue = {"steps"})
+public class CucumberTestRunner {
+
+    @BeforeClass
+    public static void initAll() {
+        Log.info("STARTING Cucumber Session");
+    }
+
+    @AfterClass
+    public static void tearDownAll() {
+        Log.info("END OF Cucumber Session");
+    }
+}
